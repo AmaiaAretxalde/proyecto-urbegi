5ca0cd16ef20df490ffc10b6dc5f2820b10bf540
eneritz5 <enebu.eb@gmail.com>
2019-12-04 16:32:21 +0100

    cambios 4 diciembre

--- src/assets/menu.js
+++ src/assets/menu.js
@@ -1,9 +1,11 @@
-const navSlide = () => {
-    const burger = document.getElementsByClassName('burger');
+export const navSlide = () => {
+    const burger = document.getElementById('burger');
     const nav = document.querySelector('.nav-links');
     const navLinks = document.querySelectorAll('.nav-links li')
 
     burger.addEventListener('click', () =>{
+        console.log('he hecho click');
+        console.log(nav.classList)
         nav.classList.toggle('nav-active');
         
         //animacion links
@@ -11,7 +13,7 @@ const navSlide = () => {
             if(link.style.animation){
                 link.style.animation = '';
             }else{
-                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 1.5}s`;
+                link.style.animation = `navLinkFade 0.5s ease forwards ${5/7 + 0.2}s`;
     
             }
         });
@@ -21,5 +23,4 @@ const navSlide = () => {
     })
 
 }
-
-navSlide();
+console.log('estoy en ');

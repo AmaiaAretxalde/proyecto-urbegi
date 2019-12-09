48edb994afc5f7b156a7f6a345c0bf084d2b3d67
eneritz5 <enebu.eb@gmail.com>
2019-12-09 09:55:37 +0100

    dia 9 diciembre

--- src/app/novedad/novedad.component.ts
+++ src/app/novedad/novedad.component.ts
@@ -1,16 +1,11 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { CargarService } from '../cargar.service';
-<<<<<<< HEAD
-<<<<<<< HEAD
-import { ComprarService } from '../comprar.service';
-=======
-import {Router} from "@angular/router";
->>>>>>> amaia
-=======
-import {Router} from "@angular/router";
+
 import { ComprarService } from '../comprar.service';
 
->>>>>>> 9e964558b277df57a3d2f49a72408e8e37d950d5
+import {Router} from "@angular/router";
+
+
 
 @Component({
   selector: 'app-novedad',
@@ -24,15 +19,10 @@ export class NovedadComponent implements OnInit {
   palabra1: string = 'navidad';
   palabra2: string = 'Christmas';
   datos: any;
-<<<<<<< HEAD
-<<<<<<< HEAD
 
-
-=======
   id:string;
   ruta:string;
   dato:string;
->>>>>>> 9e964558b277df57a3d2f49a72408e8e37d950d5
   producto:string[]=[]
   cesta:string[]=[]
 
@@ -40,34 +30,14 @@ export class NovedadComponent implements OnInit {
 
   constructor(public cargarService: CargarService, private comprarService:ComprarService, private router: Router) { }
 
-<<<<<<< HEAD
-  async anyadirALaCesta(producto){
-    console.log('añadir')
-    await this.comprarService.anyadirALaCesta(this.producto);
-  }
-=======
-  id:string;
-  ruta:string;
-  dato:string;
-  @Input() posicion: number;
-
-  constructor(public cargarService: CargarService, private router: Router) { }
->>>>>>> amaia
-=======
- 
->>>>>>> 9e964558b277df57a3d2f49a72408e8e37d950d5
-
   async ngOnInit() {
     this.datos = await this.cargarService.cargarNovedades(this.palabra1, this.palabra2)
     this.nombre = this.datos[this.posicion].name.toUpperCase();
     this.src = this.datos[this.posicion].mainImage;
     this.precio = Math.floor(this.datos[this.posicion].basePrice*100)/100;
-<<<<<<< HEAD
-<<<<<<< HEAD
+
     this.producto = this.datos[this.posicion];
-=======
-=======
->>>>>>> 9e964558b277df57a3d2f49a72408e8e37d950d5
+
     this.id = this.datos[this.posicion].id;
     this.ruta = "/producto/" + this.id;
   }
@@ -77,9 +47,7 @@ export class NovedadComponent implements OnInit {
     this.dato = await this.cargarService.mandarId(this.id);
     this.router.navigate([this.ruta])
     return this.dato;
-<<<<<<< HEAD
->>>>>>> amaia
-=======
+
     
     this.producto = this.datos[this.posicion];
 
@@ -89,7 +57,6 @@ export class NovedadComponent implements OnInit {
     console.log('añadir')
     await this.comprarService.anyadirALaCesta(this.producto);
      this.producto = this.datos[this.posicion];
->>>>>>> 9e964558b277df57a3d2f49a72408e8e37d950d5
   }
   
 }
